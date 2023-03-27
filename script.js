aa71d70c16e488c7c55aa0de27dabc5e92867a43
Renan Lessa <116324297+zankyot@users.noreply.github.com>
2023-03-27 04:47:46 +0000

    Adicionando Imagens em slide

---
+++ script.js
@@ -0,0 +1,15 @@
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function(){
+  nextImage();
+}, 3000)
+
+function nextImage(){
+  count++;
+  if(count>4){
+  count = 1;
+  }
+
+  document.getElementById("radio"+count).checked = true;
+}
